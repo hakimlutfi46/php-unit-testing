5a7d821fc67615d7f110fa5f2bf81ee3c11ae22f
hakimlutfi46 <lutfihakim1606@gmail.com>
2023-10-19 14:27:16 +0700

    adding explanation in code

--- SimpleTest.php
+++ SimpleTest.php
@@ -1,18 +1,27 @@
 <?php
-
+// Mengimpor TestCase dari PHPUnit
 use PHPUnit\Framework\TestCase;
 
+// Memuat file WordCount.php
 require_once("WordCount.php");
 
+// Membuat class SimpleTest yang merupakan turunan dari TestCase
 class SimpleTest extends TestCase
 {
+
+    // Membuat method testCountWord
     public function testCountWord()
     {
+        // Membuat object dari class WordCount
         $Wc = new WordCount();
 
+        // Membuat variabel TestSentence berisi kalimat
         $TestSentence = "My Name is Muhammad Lutfi Hakim";
+
+        // Memanggil method countWords() dari objek WordCount
         $WordCount = $Wc->countWords($TestSentence);
 
+        // Memeriksa apakah jumlah kata yang dihitung sesuai dengan yang diharapkan
         $this->assertEquals(6, $WordCount);
     }
 }
--- WordCount.php
+++ WordCount.php
@@ -1,8 +1,12 @@
 <?php
+// Membuat class bernama WordCount
 class WordCount
 {
+
+    // Membuat method countWords dengan parameter $sentence
     public function countWords($sentence)
     {
+        // mengembalikan fungsi hasil perhitungan jumlah kata. Method explode() digunakan untuk memecah string $sentence menjadi sebuah array dengan menggunakan spasi
         return count(explode(" ", $sentence));
     }
 }
